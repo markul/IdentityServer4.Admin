e3339035006c7d9da85b942295b665f0c4270807
Marat Kulmukhametov <marat.kulmukhametov@skuvault.com>
2023-03-23 14:53:39 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
+version="1.0.6"
 # build docker images according to docker-compose
 docker-compose build
 
 # rename images with following tag
-docker tag skoruba-identityserver4-admin hub.markul.net/identity:1.0.6
-docker tag skoruba-identityserver4-sts-identity hub.markul.net/sts:1.0.6
+docker tag skoruba-identityserver4-admin hub.markul.net/identity:$version
+docker tag skoruba-identityserver4-admin hub.markul.net/identity:dev
+docker tag skoruba-identityserver4-sts-identity hub.markul.net/sts:$version
+docker tag skoruba-identityserver4-sts-identity hub.markul.net/sts:dev
+docker tag skoruba-identityserver4-admin-api hub.markul.net/identity-api:$version
+docker tag skoruba-identityserver4-admin-api hub.markul.net/identity-api:dev
+
+docker image push hub.markul.net/sts:$version
+docker image push hub.markul.net/identity:$version
+docker image push hub.markul.net/identity-api:$version 
+docker image push hub.markul.net/sts:dev
+docker image push hub.markul.net/identity:dev
+docker image push hub.markul.net/identity-api:dev
